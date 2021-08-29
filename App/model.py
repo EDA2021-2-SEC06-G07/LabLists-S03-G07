0f60e4e0b8680c7804d477f4ec171fdf0edda842
B4RRET <barret50cal3011@gmail.com>
2021-08-29 14:55:54 -0500

    time file

--- App/model.py
+++ App/model.py
@@ -52,7 +52,7 @@ def newCatalog():
                'book_tags': None}
 
     catalog['books'] = lt.newList()
-    catalog['authors'] = lt.newList('ARRAY_LIST',
+    catalog['authors'] = lt.newList('SINGLE_LINKED',
                                     cmpfunction=compareauthors)
     catalog['tags'] = lt.newList('SINGLE_LINKED',
                                  cmpfunction=comparetagnames)
